0a80bb8ba8c7f7dd2651568cf041c7b6886caf9b
xrudoga <defcw58@gmail.com>
2020-06-19 16:21:02 +0800

    CMS -login

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,7 +1,18 @@
 <?php include "../includes/db.php"; ?>
+<?php ob_start(); ?>
+<?php session_start(); ?>
 <?php include "functions.php" ?>
 
-<?php ob_start(); ?>
+<?php 
+    if(isset($_SESSION['user_role'])) {
+        if($_SESSION['user_role'] !== 'admin') {
+            header("Location: ../index.php");
+        }
+    }
+    if(!isset($_SESSION['user_role'])) {
+        header("Location: ../index.php");
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,6 +17,26 @@
         <!-- /.input-group -->
     </div>
 
+    <!-- Login -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter Username">
+            </div>
+            <div class="form-group">
+                <input name="password" type="password" class="form-control" placeholder="Enter password">
+            </div>
+            <span class="input-group-btn">
+                <button class="btn btn-primary" name="login" type="submit">Submit
+                </button>
+            </span>
+        </form><!--search form-->
+        <!-- /.input-group -->
+    </div>
+
+
+
     <!-- Blog Categories Well -->
     <div class="well">
         <?php
