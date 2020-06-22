a9b9c146933e0288b7c20d3ef347fb8388a2e244
xrudoga <defcw58@gmail.com>
2020-06-22 17:17:01 +0800

    Adding views Func to posts & Resetting Views Feature

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -93,6 +93,7 @@
                 <th>Date</th>
                 <th>View Post</th>
                 <th colspan="2">Setting</th>
+                <th>Views</th>
             </tr>
         </thead>
         <tbody>
@@ -109,6 +110,7 @@
                     $post_tags = $row['post_tags'];
                     $post_comment_count = $row['post_comment_count'];
                     $post_date = $row['post_date'];
+                    $post_views_count = $row['post_views_count'];
                     echo "<tr>";
             ?>
 
@@ -138,6 +140,7 @@
                     echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                     echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete ?');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
+                    echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to reset views ?');\"  href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                     echo "</tr>";
                 }
             ?>
@@ -151,6 +154,14 @@
 
         $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
         $delete_query = mysqli_query($connection, $query); 
+        header("Location: posts.php"); exit; 
+    }
+
+    if(isset($_GET['reset'])) {
+        $the_post_id = $_GET['reset'];
+
+        $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
+        $reset_query = mysqli_query($connection, $query); 
         header("Location: posts.php"); exit; 
     }
 ?>
--- post.php
+++ post.php
@@ -15,17 +15,23 @@
             <?php
                 if(isset($_GET['p_id'])) {
                     $the_post_id = $_GET['p_id'];
-                }
 
-                $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
-                $select_all_posts_query = mysqli_query($connection,$query); 
+                    $view_query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id ";
+                    $send_query = mysqli_query($connection, $view_query);
 
-                while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];
+                    if(!$send_query) {
+                        die("query failed");
+                    }
+
+                    $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+                    $select_all_posts_query = mysqli_query($connection,$query); 
+
+                    while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = $row['post_content'];
 
             ?>
 
@@ -48,7 +54,11 @@
                     <p><?php echo $post_content ?></p>
                     <hr>
 
-                <?php } ?>
+                <?php } 
+                    } else {
+                        header("Location: index.php");
+                    }
+                ?>
 
         </div>
 
